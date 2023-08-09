27dd521457f372ce0df4a9ade927df4f9ad679e0
Dirgha Kathiriya <dirgha.ukani@gmail.com>
2023-08-09 17:33:42 +0530

    Add files

---
+++ LittelLemon/LittelLemon/LittelLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittelLemonApp.swift
+//  LittelLemon
+//
+//  Created by Dirgha Kathiriya on 2023-08-09.
+//
+
+import SwiftUI
+
+@main
+struct LittelLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
